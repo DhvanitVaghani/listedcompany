010904c7714e5968161737779c57b3453735a9a3
DhvanitVaghani <vaghanidhvanit@gmail.com>
2020-05-14 16:33:13 +0530

    commit

--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
 import 'package:listedcompany/API/companyname.dart';
 import 'package:listedcompany/API/companyprofile.dart';
 import 'package:listedcompany/Pages/co_profile.dart';
-
-
+import 'package:connectivity/connectivity.dart';
 
 class CompanyProfile extends StatefulWidget {
   @override
@@ -13,9 +15,9 @@ class CompanyProfile extends StatefulWidget {
 class _CompanyProfileState extends State<CompanyProfile> {
   List data;
   List<String> symbol;
-  
-  
-
+  Connectivity connectivity;
+  StreamSubscription<ConnectivityResult> subscription;
+  var dismiss;
   // Get name of companies from API
   void getData() async {
     GetCompanyName companyname = GetCompanyName();
@@ -23,10 +25,66 @@ class _CompanyProfileState extends State<CompanyProfile> {
     setState(() {
       data = companyname.data;
       symbol = companyname.symbol;
-      
     });
   }
 
+  void showdialog() {
+    AlertDialog alertdialog = AlertDialog(
+      title: Icon(
+        Icons.error_outline,
+        size: 30,
+        color: Colors.black,
+      ),
+      content: Text('Check Your Internet Connection'),
+      contentTextStyle: TextStyle(
+          fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+    );
+    showDialog(context: context, builder: (_) => alertdialog);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    connectivity = Connectivity();
+    subscription =
+        connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        setState(() {});
+        getData();
+      } else {
+        showdialog();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (data == null) {
+      return Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    } else {
+      return PageFragment(data: data, symbol: symbol);
+    }
+  }
+}
+
+class PageFragment extends StatelessWidget {
+  const PageFragment({
+    Key key,
+    @required this.data,
+    @required this.symbol,
+  }) : super(key: key);
+
+  final List data;
+  final List<String> symbol;
   void showdata(index, BuildContext context) async {
     // Get Company Profiles from API
     FetchProfile companyProfile = FetchProfile(symbol[index]);
@@ -45,59 +103,46 @@ class _CompanyProfileState extends State<CompanyProfile> {
   }
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getData();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (data == null) {
-      return Scaffold(
-        body: Center(child: CircularProgressIndicator()),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                showSearch(
-                    context: context,
-                    delegate: CompanySearch(
-                        companyname: data,
-                        ));
-              })
-        ], centerTitle: true, title: Text('Listed Company In Market')),
-        body: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                margin: EdgeInsets.all(8),
-                child: ListTile(
-                  title: Text(
-                    data[index]["name"],
-                    style: TextStyle(color: Colors.black, fontSize: 20.0),
-                  ),
-                  subtitle:Text( symbol[index]),
-                  onTap: () {
-                    showdata(index, context);
-                  },
+    return Scaffold(
+      appBar: AppBar(actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                  context: context,
+                  delegate: CompanySearch(
+                    companyname: data,
+                  ));
+            })
+      ], centerTitle: true, title: Text('Listed Company In Market')),
+      body: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              margin: EdgeInsets.all(8),
+              child: ListTile(
+                title: Text(
+                  data[index]["name"],
+                  style: TextStyle(color: Colors.black, fontSize: 20.0),
                 ),
-              );
-            }),
-      );
-    }
+                subtitle: Text(symbol[index]),
+                onTap: () {
+                  showdata(index, context);
+                },
+              ),
+            );
+          }),
+    );
   }
 }
 
 class CompanySearch extends SearchDelegate<String> {
   List companyname;
- 
+
   CompanySearch({this.companyname});
 
-  void showdata(BuildContext context,String sym) async {
+  void showdata(BuildContext context, String sym) async {
     // Get Company Profiles from API
     FetchProfile companyProfile = FetchProfile(sym);
     await companyProfile.getProfile();
@@ -142,17 +187,20 @@ class CompanySearch extends SearchDelegate<String> {
   Widget buildResults(BuildContext context) {
     // TODO: implement buildResults
     return ListTile(
-      title: Text("No Record Found"+" "+"$query"),
+      title: Text("No Record Found" + " " + "$query"),
       onTap: () {
-       // showdata(index, context);
+        // showdata(index, context);
       },
     );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final suggestionCompany = query.isEmpty? companyname:
-        companyname.where((p) => p["name"].toString().startsWith(query)).toList();
+    final suggestionCompany = query.isEmpty
+        ? companyname
+        : companyname
+            .where((p) => p["name"].toString().startsWith(query))
+            .toList();
     return ListView.builder(
         itemCount: suggestionCompany.length,
         itemBuilder: (BuildContext context, int index) {
@@ -161,22 +209,22 @@ class CompanySearch extends SearchDelegate<String> {
             child: ListTile(
               title: RichText(
                   text: TextSpan(
-                      text: suggestionCompany[index]["name"].substring(0, query.length),
+                      text: suggestionCompany[index]["name"]
+                          .substring(0, query.length),
                       style: TextStyle(
                           color: Colors.black, fontWeight: FontWeight.bold),
                       children: [
                     TextSpan(
-                        text: suggestionCompany[index]["name"].substring(query.length),
+                        text: suggestionCompany[index]["name"]
+                            .substring(query.length),
                         style: TextStyle(color: Colors.grey))
                   ])),
-                  subtitle: Text(suggestionCompany[index]["symbol"]),
+              subtitle: Text(suggestionCompany[index]["symbol"]),
               onTap: () {
-                showdata(context,suggestionCompany[index]["symbol"]);
+                showdata(context, suggestionCompany[index]["symbol"]);
               },
             ),
           );
         });
   }
 }
-
-
